717be1a8032fd8f2bacf8fbaea5e06b9866dfb51
plz09 <62223358+plz09@users.noreply.github.com>
2020-12-07 13:50:58 +0000

    Cadastro de agendamento e validacão de data

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
-import express, { response } from 'express';
+import express from 'express';
+
+// src/server.ts
+import routes from './routes';
 
 const app = express();
 
-app.get('/', (request, response) => {
-    return response.json({ message: 'Hello GoStack' });
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333, () => {
-    console.log('Server running on port 3333');
+  console.log('Server running on port 3333');
 });
+
